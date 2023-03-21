fde246b539c6592e3722d865022e066fe489f64d
Ayaz <ayazmahmood394@gmail.com>
2023-03-21 22:23:08 +0500

    change

--- src/redux/apiCalls.js
+++ src/redux/apiCalls.js
@@ -5,7 +5,7 @@ import axios from "axios";
 export const login = async (dispatch, user) =>{
     dispatch(loginStart());
     try{
-        const res = await axios.post("http://localhost:4000/api/auth/login",user);
+        const res = await axios.post("https://ecommerce-two-ruddy.vercel.app/api/auth/login",user);
         dispatch(loginSuccess(res.data));
     }catch(err){
         dispatch(loginFailure());
